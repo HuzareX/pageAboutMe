9bbdd21dfd0bf1e111e72d510b830733ff02d27e
HuzareX <danielwegierski@gmail.com>
2023-03-04 18:51:28 +0100

    Navbar done, added new section

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
+//---NAVBAR---
 
+const mobileNav = document.querySelector('.mobile-nav')
+const burgetBtn = document.querySelector('.fa-bars')
+const closeBtn = document.querySelector('.fa-xmark')
+const mobileNavItems = document.querySelectorAll('.mobile-nav-to-section')
+
+
+
+const showMobileNav = () => {
+    mobileNav.style.right = "0"
+}
+
+const closeMobileNav = (e) => {
+    mobileNav.style.right = "-150%"
+    
+}
+
+burgetBtn.addEventListener('click', showMobileNav)
+closeBtn.addEventListener('click', closeMobileNav)
+mobileNavItems.addEventListener('click', closeMobileNav)
